7fe446bb0ec645f814675b2bef81a6cc2d621186
Matt Braddock <matt.braddock@gmail.com>
2025-03-04 14:01:38 -0500

    interface export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { SingleElimination } from './SingleElimination.js';
 import { DoubleElimination } from './DoubleElimination.js';
 import { RoundRobin } from './RoundRobin.js';
 import { Stepladder } from './Stepladder.js';
-import { Swiss, Player } from './Swiss.js';
+import { Swiss } from './Swiss.js';
 
-export { SingleElimination, DoubleElimination, RoundRobin, Stepladder, Swiss, Player};
+export { SingleElimination, DoubleElimination, RoundRobin, Stepladder, Swiss };
---
+++ src/interfaces.ts
@@ -0,0 +1,4 @@
+import { Match } from './Match.js';
+import { Player } from './Swiss.js';
+
+export { Match, Player };
